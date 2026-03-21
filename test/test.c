a2e75c05f290d97bb16b9f7967f1e14241575424
kn5suzuki <kn5suzuki2000@gmail.com>
2026-03-21 14:42:06 +0900

    completion of step0

--- test/test.c
+++ test/test.c
@@ -54,6 +54,12 @@ cleanup(void)
 static int
 app_main(void)
 {
+    debugf("press Ctrl+C to terminate");
+    while (!terminate)
+    {
+        sleep(1);
+    }
+    debugf("terminate");
     return 0;
 }
 
