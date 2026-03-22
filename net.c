36eea87da88f573df3408d52601da52d3238938f
kn5suzuki <kn5suzuki2000@gmail.com>
2026-03-22 23:55:22 +0900

    completion of step1

--- net.c
+++ net.c
@@ -1,42 +1,130 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
 #include "platform.h"
 
 #include "util.h"
+#include "net.h"
 
-int
-net_init(void)
+static struct net_device *devices;
+
+struct net_device *
+net_device_alloc(void)
 {
-    infof( "initialize...");
+    struct net_device *dev;
+
+    dev = memory_alloc(sizeof(*dev));
+    if (!dev)
+    {
+        errorf("memory_alloc() failure");
+        return NULL;
+    }
+    return dev;
+}
+
+/*
+ * NOTE: must not be called after net_run()
+ */
+int net_device_register(struct net_device *dev)
+{
+    static unsigned int index = 0;
+
+    dev->index = index++;
+    snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+    dev->next = devices;
+    devices = dev;
+    infof("success, dev=%s, type=0x%04x", dev->name, dev->type);
+    return 0;
+}
+
+static int
+net_device_open(struct net_device *dev)
+{
+    infof("dev=%s", dev->name);
+    if (NET_DEVICE_IS_UP(dev))
+    {
+        errorf("already opened, dev=%s", dev->name);
+        return -1;
+    }
+    dev->flags |= NET_DEVICE_FLAG_UP;
+    return 0;
+}
+
+static int
+net_device_close(struct net_device *dev)
+{
+    infof("dev=%s", dev->name);
+    if (!NET_DEVICE_IS_UP(dev))
+    {
+        errorf("not opened, dev=%s", dev->name);
+        return -1;
+    }
+    dev->flags &= ~NET_DEVICE_FLAG_UP;
+    return 0;
+}
+
+int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
+{
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    if (!NET_DEVICE_IS_UP(dev))
+    {
+        errorf("not opened, dev=%s", dev->name);
+        return -1;
+    }
+    if (dev->mtu < len)
+    {
+        errorf("too long, dev=%s, mtu=%u, len=%zu", dev->name, dev->mtu, len);
+        return -1;
+    }
+    return 0;
+}
+
+int net_init(void)
+{
+    infof("initialize...");
     if (platform_init() == -1)
     {
-        errorf( "platform_init() failure");
+        errorf("platform_init() failure");
         return -1;
     }
-    infof( "success");
+    infof("success");
     return 0;
 }
 
-int
-net_run(void)
+int net_run(void)
 {
-    infof( "startup...");
+    struct net_device *dev;
+
+    infof("startup...");
     if (platform_run() == -1)
     {
-        errorf( "platform_run() failure");
+        errorf("platform_run() failure");
         return -1;
     }
-    infof( "success");
+    for (dev = devices; dev; dev = dev->next)
+    {
+        net_device_open(dev);
+    }
+    infof("success");
     return 0;
 }
 
-int
-net_shutdown(void)
+int net_shutdown(void)
 {
-    infof( "shutting down...");
+    struct net_device *dev;
+
+    infof("shutting down...");
     if (platform_shutdown() == -1)
     {
-        warnf( "platform_shutdown() failure");
+        warnf("platform_shutdown() failure");
         return -1;
     }
-    infof( "success");
+    for (dev = devices; dev; dev = dev->next)
+    {
+        net_device_close(dev);
+    }
+    infof("success");
     return 0;
 }
