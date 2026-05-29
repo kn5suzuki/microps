055ccfb486fdf486d88aaf829da7de820d8bd033
kn5suzuki <kn5suzuki2000@gmail.com>
2026-05-29 09:28:38 +0900

    completion of step20

--- net.c
+++ net.c
@@ -11,6 +11,7 @@
 #include "ip.h"
 #include "icmp.h"
 #include "udp.h"
+#include "tcp.h"
 
 struct net_protocol
 {
@@ -317,6 +318,11 @@ int net_init(void)
     if (udp_init() == -1)
     {
         errorf("udp_init() failure");
+        return -1;
+    }
+    if (tcp_init() == -1)
+    {
+        errorf("tcp_init() failure");
         return -1;
     }
     infof("success");
